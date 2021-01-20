2a0dc1acf590206ecd5c6b81160583f89ab852dc
Adeepa <adeepa91@gmail.com>
2021-01-20 23:00:32 +0530

    responce changed

--- src/main/java/com/student/info/controller/TeacherController.java
+++ src/main/java/com/student/info/controller/TeacherController.java
@@ -3,6 +3,8 @@ package com.student.info.controller;
 import com.student.info.model.Teacher;
 import com.student.info.services.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,8 +22,9 @@ public class TeacherController {
     }
 
     @PostMapping(value = "/save")
-    public void saveOrUpdateTeacher(@RequestBody Teacher teacher){
+    public ResponseEntity<String> saveOrUpdateTeacher(@RequestBody Teacher teacher){
         teacherService.saveOrUpdateTeacher(teacher);
+        return new ResponseEntity<>("Successfully Added", HttpStatus.OK);
     }
 
 }
